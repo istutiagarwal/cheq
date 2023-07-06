612a60fe3d9805d71831b0e198177d5187ff3750
istuti agarwal <istuti.agarwal109@example.com>
2023-07-07 01:29:24 +0530

    bottom nav bar

---
+++ app/src/main/java/com/example/cheqq/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.cheqq.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cheqq.R
+import com.example.cheqq.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val newsNavHostFragment =supportFragmentManager.findFragmentById(R.id.newsNavHostFragment)
+        //binding.bottomNavigationView.setupWithNavController(binding.flFragment.findViewById(R.id.bottomNavigationView).f)
+        if (newsNavHostFragment != null) {
+            binding.bottomNavigationView.itemIconTintList = null
+            binding.bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+        }
+
+    }
+}
